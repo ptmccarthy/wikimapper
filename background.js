46322d76e28e4b254adca4471cad7f494d94d298
Patrick McCarthy <p.t.mccarthy@gmail.com>
2013-05-14 14:46:12 -0700

    Migrated to using WebSQL

--- background.js
+++ background.js
@@ -1,8 +1,20 @@
 // Listens for URL changes of any tabs and if wikipedia gathers info from them.
 
 // store wikipedia title tag to strip out later
-var storage = chrome.storage.local;
 var titleTag = ' - Wikipedia, the free encyclopedia';
+
+// create database if does not already exist
+var db = window.openDatabase(
+	'WikiMapper',			// db name
+	'0.1',						// version
+	'wikimapper',			// description
+	5*1024*1024			// db size in bytes
+);
+
+// create table if does not already exist
+db.transaction(function (tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS PAGES (title, url, ref, date)');
+});
 
 
 function checkForWikiUrl(tabId, changeInfo, tab) {
@@ -24,9 +36,10 @@ function recordPageData(response) {
 	page.title = response.title.replace(titleTag, "");
 	page.url = response.url;
 	page.ref = response.ref;
-	storage.set(page, function() {
-		console.log('Saved', page);
-	});
+	page.date = Date.now();
+	db.transaction(function (tx) {
+		tx.executeSql('INSERT INTO PAGES (title,url,ref,date) VALUES (?,?,?,?)', [page.title, page.url, page.ref, page.date]);
+	})	
 }
 
 // Listen for any changes to the URL of any tab.
--- render.js
+++ render.js
@@ -1,7 +1,27 @@
 // Script that does the querying and rendering of data
 
-var storage = chrome.storage.local;
+var db = openDatabase(
+	'WikiMapper',
+	'0.1',
+	'wikimapper',
+	5*1024*1024
+);
 
-storage.get(['title', 'url', 'ref'], function(items) {
-	console.log(items.title, items.url, items.ref);
+db.readTransaction(function (tx) {
+	tx.executeSql('SELECT * FROM pages', [], function(tx, results) {
+		var len = results.rows.length;
+		var i;
+		for(i=0; i<len; i++) {
+			console.log("Title: " + results.rows.item(i).title +
+									" URL: " + results.rows.item(i).url +
+									" Referer: " + results.rows.item(i).ref +
+									" UTC: " + results.rows.item(i).date);
+		}
+	})
 })
+
+//var storage = chrome.storage.local;
+//
+//storage.get(['title', 'url', 'ref', 'date'], function(items) {
+//	console.log(items.title, items.url, items.ref, items.date);
+//})

3cf3e7d1965cd514b96d3dc44441970077f7610f
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-01-16 19:12:18 -0800

    Removed verbose debug logging

--- background.js
+++ background.js
@@ -19,15 +19,12 @@ function sessionHandler(commitData) {
 	var session = findSessionOf(commitData);
 
 	createPageObject(session, commitData, function(page) {
-		console.log('evaluate ' + JSON.stringify(page));
 		if (page.id == 1) {
 			recordRoot(page);
 		} else {
 			recordChild(page);	
 		}
 	});
-
-	console.log("Sessions: " + JSON.stringify(sessions));
 }
 
 // Session Finder
@@ -42,12 +39,10 @@ function findSessionOf(commitData) {
 	var parentNode;
 	sessions.forEach(function(session) {
 		if (session.tabs.indexOf(commitData.tabId) >= 0) {
-			console.log('found self ' + commitData.tabId);
 			ret.id = session.id;
 			ret.parentNode = tabStatus[commitData.tabId].id;
 		}
 		else if(commitData.parentId !== undefined) {
-			console.log('looking for parent ' + commitData.parentId);
 			var index = session.tabs.indexOf(commitData.parentId)
 			if (index >= 0) {
 				console.log('found parent');
@@ -59,7 +54,6 @@ function findSessionOf(commitData) {
 	})
 
 	if (ret.id != "") {
-		console.log('returning session ' + JSON.stringify(ret));
 		return ret;
 	}
 	else {
@@ -75,14 +69,12 @@ function createNewSession(commitData) {
 						"tabs": [ commitData.tabId ],
 						"nodeIndex": 1,
 				}
-	console.log('creating session:' + JSON.stringify(session))
 	sessions.push(session);
 	return { "id": session.id, "parentNode": "" };
 }
 
 
 function createPageObject(session, commitData, callback) {
-	console.log('creating page object');
 	sessions.forEach(function(activeSession) {
 		if (activeSession.id == session.id) {
 			var page = { 	"id": activeSession.nodeIndex,
@@ -103,7 +95,6 @@ function createPageObject(session, commitData, callback) {
 }
 
 function recordRoot(page) {
-	console.log('root');
 	// set the tabStatus of this tabId to the page content
 	setTabStatus(page.data.tabId, page);
 	// record new session and root node to localStorage
@@ -111,20 +102,18 @@ function recordRoot(page) {
 }
 
 function recordChild(page) {
-	console.log('child');
 	// set the tabStatus of this tabId to the page content
 	setTabStatus(page.data.tabId, page);
 	// retrieve the tree for this session from localStorage
 	var tree = JSON.parse(localStorage.getItem(page.data.sessionId));
 	// find the parent node
 	var parent = findNode(tree, page.data.parentId);
-	console.log('found node ' + parent);
 	// add this node as a child
 	parent.children.push(page);
-	console.log('modified node ' + JSON.stringify(parent));
 	// record the modified tree to localStorage
 	localStorage.setItem(page.data.sessionId, JSON.stringify(tree));
 }
+)
 
 // Find Node
 // recursively look through a JSON tree for the specified node and return it
@@ -186,7 +175,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, response) {
 			response();
 		break;
 
-		// page requesting json tree object
+		// tree page requesting json tree object
 		case "load":
 			response(selectedTree);
 		break;
@@ -221,7 +210,6 @@ chrome.webNavigation.onCommitted.addListener(function(details) {
 		// get the parent tab id of the tab that the nav event occurs in
 		// and add it as an additional key in commitData
 		var commitData = details;
-		console.log(commitData);
 		chrome.tabs.get(details.tabId, function(tab) {
 			commitData.parentId = tab.openerTabId;
 			sessionHandler(commitData);

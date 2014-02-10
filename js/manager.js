56bb828bac9abe0a1c76a920e1611c31403c57d6
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-02-10 15:10:27 -0800

    Delete manager, not being used

--- js/manager.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded'), function() {
-	document.querySelector('button').addEventListener('click', clickHandler);
-}
-
-function clickHandler() {
-	localStorage.clear();
-}

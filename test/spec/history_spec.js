cb69ac2cfb7eb730816f0d2360f2e0ba1321d0c6
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-02-15 15:07:58 -0800

    Some basic unit tests for history.js

--- test/spec/history_spec.js
+++ test/spec/history_spec.js
@@ -1,5 +1,42 @@
 describe("history", function() {
-	it("expects true to be true", function() {
-		expect(true).toBe(true);
+	beforeEach(function() {
+		chrome = {
+			runtime: {
+				sendMessage: function(){},
+			}
+		}
+
+		// retrieve fake localStorage data from support files
+		// note: do this synchronously
+		var localStoarge = (function () {
+	    var localStorage = null;
+	    $.ajax({
+	        'async': false,
+	        'global': false,
+	        'url': 'support/localStorage.json',
+	        'dataType': "json",
+	        'success': function (data) {
+	            localStorage = data;
+	        }
+	    });
+	    return localStorage;
+		})(); 
+
+		spyOn(chrome.runtime, "sendMessage").and.returnValue(localStorage);
 	});
+
+	it("expects to send a message requesting localStorage", function(done) {
+		require(['../js/history'], function() {
+			expect(chrome.runtime.sendMessage).toHaveBeenCalledWith({payload: "localStorage"}, jasmine.any(Function));
+			done();
+		});
+	});
+
+	it("expects to receive a response containing localStorage data", function(done) {
+		require(['../js/history'], function() {
+			expect(localStorage).toBe(localStorage);
+			done();
+		})
+	});
+
 });

db2f1b98c4a4af04209cb00c4e3d840c7a8a6b8c
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-03-05 15:49:45 -0800

    Add some unit tests for background.js

--- test/spec/background_spec.js
+++ test/spec/background_spec.js
@@ -1,5 +1,28 @@
 describe("background", function() {
-	it("expects true to be true", function() {
-		expect(true).toBe(true);
-	});
+
+  beforeEach(function() {
+    spyOn(chrome.runtime.onMessage, "addListener").and.callThrough();
+    spyOn(chrome.webNavigation.onCommitted, "addListener").and.callThrough();
+    spyOn(chrome.browserAction.onClicked, "addListener").and.callThrough();
+  });
+
+  it("expects to set up event listeners", function(done) {
+    require(['../background'], function() {
+      expect(chrome.runtime.onMessage.addListener).toHaveBeenCalled();
+      expect(chrome.webNavigation.onCommitted.addListener).toHaveBeenCalled();
+      expect(chrome.browserAction.onClicked.addListener).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it("expects to create necessary data structures", function(done) {
+    require(['../background'], function() {
+      expect(sessions == []);
+      expect(tabStatus == {});
+      expect(data == {});
+      expect(selectedTree == {});
+      done();
+    })
+  });
+
 });

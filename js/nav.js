e97f3018e64b14cab39d51e9832ac9f72fe28aec
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-09-26 14:34:45 -0700

    Fix #69 and clean deprecated nav code

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,14 @@
 
 // load default on document ready event and start nav function
 $(document).ready(function() {
-  chrome.runtime.sendMessage({ payload: "set" }, function(response) {
-    if (response.name === null) {
-      $('#about').attr("state", "active");
-      $('#content').load('about.html').show();
+  chrome.runtime.sendMessage({ payload: 'set' }, function(response) {
+    if (response.name == null) {
+      $('#about').attr('state', 'active');
+      $('#content').load('about.html');
     }
     else {
-      $('#content').load('tree.html').show();
+      $('#tree').attr('state', 'active');
+      $('#content').load('tree.html');
     }
 
     nav();
@@ -22,31 +23,4 @@ function nav() {
 
     $('#content').load($(this).attr('id') + '.html');
   });
-}
-
-
-// navigation button listener function
-// changes content displayed in content div
-function oldnav() {
-  $('#about').click(function() {
-    deactivateAll();
-    $(this).attr("state", "active");
-    $('#content').load('about.html');
-  });
-
-  $('#current').click(function() {
-    deactivateAll();
-    $(this).attr("state", "active");
-    chrome.runtime.sendMessage({ payload: "set"}, function() {      
-      $('#content').load('tree.html');
-    });
-  });
-
-  $('#show-history').click(function() {
-    deactivateAll();
-    $(this).attr("state", "active");
-    $('#content').load('history.html'); 
-  });
-
-  $('#nav').show();
 }

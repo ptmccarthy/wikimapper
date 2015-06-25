07f8b1abce491f97c339589279ea2559767da6e4
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-06-24 22:22:15 -0700

    Ensure navigation continues to display on page refresh.

--- src/web/js/router.js
+++ src/web/js/router.js
@@ -16,29 +16,37 @@ var HistoryView = require('./views/history');
 module.exports = Backbone.Router.extend({
 
   routes: {
-    '': 'index',
+    '': 'title',
     title: 'title',
     current: 'current',
     history: 'history'
   },
 
-  index: function() {
-    if (!App.nav) {
-      App.nav = new NavView({});
-      App.showNavigation(App.nav);
-    }
-  },
-
   title: function() {
+    this.ensureNav();
     App.showBody(new TitleView({}));
   },
 
   current: function() {
+    this.ensureNav();
     App.showBody(new CurrentView({}));
   },
 
   history: function() {
+    this.ensureNav();
     App.showBody(new HistoryView({}));
+  },
+
+  /**
+   * Ensure that the navigation view is displayed.
+   * This is mostly to ensure that navigation doesn't disappear if a page
+   * is manually refreshed (with its '#page' URL)
+   */
+  ensureNav: function() {
+    if (!App.nav) {
+      App.nav = new NavView({});
+      App.showNavigation(App.nav);
+    }
   }
 
 });

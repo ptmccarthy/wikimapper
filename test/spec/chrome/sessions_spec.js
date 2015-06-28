def294d764b79e0a01e85b4a79711f3bb40a7442
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-06-28 16:15:53 -0700

    Add some more sessions api tests

--- test/spec/chrome/sessions_spec.js
+++ test/spec/chrome/sessions_spec.js
@@ -3,14 +3,19 @@
 var App =      require('../../../src/chrome/background'),
     Sessions = require('../../../src/chrome/session-handler');
 
-describe('Background initialization', function() {
+describe('Session handler', function() {
 
   beforeAll(function() {
     App.initialize();
   });
 
+  beforeEach(function() {
+    Sessions.activeSessions = [];
+    Sessions.tabStatus = {};
+  });
+
   it('should have no existing tab status', function() {
-    expect(Sessions.tabStatus).toEqual([]);
+    expect(Sessions.tabStatus).toEqual({});
   });
 
   it('should have no existing active sessions', function() {
@@ -37,6 +42,14 @@ describe('Background initialization', function() {
     expect(Sessions.activeSessions = []);
   });
 
+  it('should be able to process navigation events', function() {
+    spyOn(window.chrome.tabs, 'get');
+    var details = {tabId: 566, openerId: 420};
+
+    Sessions.processNavigation(details);
+    expect(window.chrome.tabs.get).toHaveBeenCalledWith(details.tabId, jasmine.any(Function));
+  });
+
   it('should be able to create a session', function() {
     var newSession;
     var commitData = {
@@ -49,6 +62,33 @@ describe('Background initialization', function() {
     expect(typeof newSession.id).toBe('number');
     expect(newSession.tabs).toContain(567);
     expect(newSession.nodeIndex).toEqual(1);
+  });
+
+  it('should be able to find an existing session', function() {
+    var commitData = {tabId: 3};
+    var session;
+
+    Sessions.activeSessions = [
+      {id: 501, tabs: [1, 2, 3]},
+      {id: 650, tabs: [4, 5]}
+    ];
+
+    Sessions.tabStatus = {
+      1: {id: 3},
+      2: {id: 4},
+      3: {id: 500},
+      4: {id: 204},
+      5: {id: 7}
+    };
+
+    // find a session in the same tab
+    session = Sessions.findSessionOf(commitData);
+    expect(session.id).toEqual(501);
+
+    // find a session in a parent tab
+    commitData = { tabId: 8, parentId: 5 };
+    session = Sessions.findSessionOf(commitData);
+    expect(session.id).toEqual(650);
   });
 
 });

bbed27a5270422b5731858dd3ee696ec8bf5d2a8
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-06-28 03:10:56 -0700

    Add some tests for session-handler

--- test/spec/chrome/sessions_spec.js
+++ test/spec/chrome/sessions_spec.js
@@ -17,4 +17,38 @@ describe('Background initialization', function() {
     expect(Sessions.activeSessions).toEqual([]);
   });
 
+  it('should be able to clear a session', function() {
+    Sessions.activeSessions = [{ id: 123 }, { id: 543} , { id: 998} ];
+
+    Sessions.clearSession(543);
+    expect(Sessions.activeSessions).toEqual([{ id: 123}, { id: 998} ]);
+
+    Sessions.clearSession(123);
+    expect(Sessions.activeSessions).toEqual([{ id: 998 }]);
+
+    Sessions.clearSession(998);
+    expect(Sessions.activeSessions).toEqual([]);
+  });
+
+  it('should be able to clear all sessions', function() {
+    Sessions.activeSessions = [{ id: 123 }, { id: 543} , { id: 998} ];
+
+    Sessions.clearAllSessions();
+    expect(Sessions.activeSessions = []);
+  });
+
+  it('should be able to create a session', function() {
+    var newSession;
+    var commitData = {
+      tabId: 567
+    };
+
+    Sessions.createNewSession(commitData);
+    newSession = Sessions.activeSessions[0];
+
+    expect(typeof newSession.id).toBe('number');
+    expect(newSession.tabs).toContain(567);
+    expect(newSession.nodeIndex).toEqual(1);
+  });
+
 });

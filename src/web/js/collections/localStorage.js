6ae4c515f592b7b1a663fe093f7732fd5b315a29
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-12-03 13:54:30 -0800

    Clean up comments in localStorage collection interface

--- src/web/js/collections/localStorage.js
+++ src/web/js/collections/localStorage.js
@@ -13,8 +13,8 @@ module.exports = Backbone.Collection.extend({
   sortingField: 'id',
   sortAscending: false,
 
-  // default comparator is id (date) descending
   comparator: function(model) {
+    // default comparator is id (date) descending
     return -model.get('id');
   },
 
@@ -22,6 +22,10 @@ module.exports = Backbone.Collection.extend({
     this.localStorage = window.localStorage;
   },
 
+  /**
+   * Custom Fetch
+   * Retrieves localStorage and converts it to a Backbone.Collection
+   */
   fetch: function() {
     var history = [];
     var session = {};
@@ -38,6 +42,11 @@ module.exports = Backbone.Collection.extend({
     this.trigger('sync');
   },
 
+  /**
+   * Custom Parse
+   * Add localStorage objects into the collection
+   * @param history - localStorage parsed into an Array of objects
+   */
   parse: function(history) {
     _.each(history, _.bind(function(session) {
       session.checked = false;
@@ -45,6 +54,10 @@ module.exports = Backbone.Collection.extend({
     }, this));
   },
 
+  /**
+   * Return the most recent session model.
+   * @returns {Backbone.Model}
+   */
   getLatest: function() {
     this.fetch();
 
@@ -53,6 +66,10 @@ module.exports = Backbone.Collection.extend({
     });
   },
 
+  /**
+   * Set the sorting on the collection by assigning a new comparator function.
+   * @param sortBy
+   */
   setSortBy: function(sortBy) {
     var direction;
 
@@ -132,7 +149,7 @@ module.exports = Backbone.Collection.extend({
 
         if (child.name.toLowerCase().indexOf(searchTerm) < 0) {
           if (child.children && child.children.length > 0) {
-            containsTerm = searchChildren(child.children, searchTerm)
+            containsTerm = searchChildren(child.children, searchTerm);
           }
         } else {
           containsTerm = true;

2364fa6cac181e3be2c617c394be6fde24fefc0f
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-12-06 20:43:42 -0800

    Update tests & resources for searching

---
+++ test/resources/commit_data.js
@@ -0,0 +1,12 @@
+var data = {
+  "frameId": 0,
+  "processId": 3190,
+  "tabId": 2537,
+  "timeStamp": 1449463219448.691,
+  "transitionQualifiers": [],
+  "transitionType": "link",
+  "url": "https://en.wikipedia.org/wiki/Sooners",
+  "openerTabId": 2529
+};
+
+module.exports = data;
--- test/spec/chrome/storage_spec.js
+++ test/spec/chrome/storage_spec.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var Storage = require('../../../src/chrome/storage');
+var Storage =     require('../../../src/chrome/storage');
+var mockSession = require('../../resources/mock_session');
 
 describe('Storage API', function() {
 
-  beforeAll(function() {
-    // TODO: set up spies
-  });
+  it('should be able to detect a search results page and name it appropriately', function() {
+    var searchURL = 'https://en.wikipedia.org/wiki/Special:Search?search=_&go=Go';
+    var trimmedURL = Storage.shortenURL(searchURL);
 
-  it('should be able to create page objects for storing', function() {
-    // TODO: mock out a session and some commitdata to test the function
+    expect(trimmedURL).toEqual('Search Results: _');
   });
 
   it('should be able to derive the page name from Wikipedia URLs accurately', function() {
@@ -19,4 +19,14 @@ describe('Storage API', function() {
     expect(trimmedURL).toEqual('JavaScript\'s Foo bar');
   });
 
+  it('should be able to search children nodes by their ID', function() {
+    var node = Storage.findNode(mockSession, 8);
+    expect(node.name).toEqual('Dutch language');
+    node = Storage.findNode(mockSession, 19);
+    expect(node.name).toEqual('Chamber of Regions');
+    node = Storage.findNode(mockSession, 57);
+    expect(node).toBeUndefined();
+    node = Storage.findNode(mockSession, -5);
+    expect(node).toBeUndefined();
+  });
 });

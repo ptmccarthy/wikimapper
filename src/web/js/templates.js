2fff56105724b15c2c075f93bc1a504df4cd3e4f
Patrick McCarthy <p.t.mccarthy@gmail.com>
2025-03-31 19:33:48 -0700

    Refactor templates to ES module

--- src/web/js/templates.js
+++ src/web/js/templates.js
@@ -1,11 +1,17 @@
-export default {
+import navigation from '../templates/navigation.hbs';
+import title from '../templates/title.hbs';
+import latest from '../templates/latest.hbs';
+import history from '../templates/history.hbs';
+import historyItem from '../templates/history-item.hbs';
+import historyTable from '../templates/history-table.hbs';
 
-  navigation: require('../templates/navigation.hbs'),
-  title: require('../templates/title.hbs'),
-  latest: require('../templates/latest.hbs'),
-  history: require('../templates/history.hbs'),
-  historyItem: require('../templates/history-item.hbs'),
-  historyTable: require('../templates/history-table.hbs'),
+const templates = {
+  navigation,
+  title,
+  latest,
+  history,
+  historyItem,
+  historyTable,
 
   get: function(template) {
     if (template in this) {
@@ -14,5 +20,6 @@ export default {
       console.error('Failed to load template: ' + template);
     }
   }
-
 };
+
+export default templates;

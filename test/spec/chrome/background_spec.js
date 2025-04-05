cecc9824b29883c3fa0615f56af5543de24f6434
Patrick McCarthy <p.t.mccarthy@gmail.com>
2025-04-04 23:14:24 -0700

    Update tests to work with session storage

--- test/spec/chrome/background_spec.js
+++ test/spec/chrome/background_spec.js
@@ -1,6 +1,7 @@
 import sinon from 'sinon';
 import { initialize } from '../../../src/chrome/background.js';
 import browser from 'webextension-polyfill';
+import Sessions from '../../../src/chrome/session-handler.js';
 
 describe('Background Script', () => {
   let sandbox;
@@ -13,20 +14,24 @@ describe('Background Script', () => {
     browser.runtime.onMessage.addListener = sandbox.spy();
     browser.runtime.onInstalled.addListener = sandbox.spy();
     browser.action.onClicked.addListener = sandbox.spy();
+
+    // Mock Sessions.initialize
+    sandbox.stub(Sessions, 'initialize').resolves();
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should set up all event listeners', () => {
-    initialize();
+  it('should set up all event listeners', async() => {
+    await initialize();
 
     // Check that all event listeners are set up
     expect(browser.webNavigation.onCommitted.addListener.called).toBe(true);
     expect(browser.runtime.onMessage.addListener.called).toBe(true);
     expect(browser.runtime.onInstalled.addListener.called).toBe(true);
     expect(browser.action.onClicked.addListener.called).toBe(true);
+    expect(Sessions.initialize.called).toBe(true);
   });
 
   // TODO: test event filter
